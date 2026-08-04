4c26caee1d15cb3400bf248ff280eff539ee6c46
shahdsaleem <shahdsaleem5722@gmail.com>
2026-08-05 00:24:25 +0300

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-#from fastapi import FastAPI
-#app = FastAPI()
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+app = FastAPI()
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Inserting in a database",
+        "done": False
+    },
+
+    {
+        "id": 2,
+        "title": "defining a function",
+        "done": True
+    },
+
+    {
+        "id": 3,
+        "title": "printing a result",
+        "done": True
+    }
+]
 
 #@app.get('/')
 #def hello_world():
 #    return {"message": 'Hello World'}
-
-from fastapi import FastAPI
-app = FastAPI()
+# Stage 0 DONE
 
 @app.get("/")
 def api_description():
@@ -25,3 +44,21 @@ def health_description():
 # git add .
 # git commit -m "Stage 1: root and health endpoints"
 # git push
+
+@app.get('/tasks')
+def return_tasks():
+    return tasks
+
+@app.get('/tasks/{id}')
+def task_with_id(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+        
+    return JSONResponse(
+        status_code = 404,
+        content = {"error": f"Task {id} not found"}
+    )
+
+# git add .
+# git commit -m "Stage 2: read endpoints with 404"
