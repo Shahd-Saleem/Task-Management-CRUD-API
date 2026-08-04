68dc625d856fbf93af7f06a03a3759a4323e20b7
shahdsaleem <shahdsaleem5722@gmail.com>
2026-08-04 23:39:38 +0300

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+#from fastapi import FastAPI
+#app = FastAPI()
+
+#@app.get('/')
+#def hello_world():
+#    return {"message": 'Hello World'}
+
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get('/')
-def hello_world():
-    return {"message": 'Hello World'}
+@app.get("/")
+def api_description():
+    return{
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": ["/tasks"]
+    }
+
+@app.get('/health')
+def health_description():
+    return{
+        "status": "ok" 
+    }
+
+# git add .
+# git commit -m "Stage 1: root and health endpoints"
+# git push
